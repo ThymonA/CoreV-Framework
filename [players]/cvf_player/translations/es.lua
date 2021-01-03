52c0e5ca9f2c898518421f8ac01f7eda0ff7e083
Valdorz <68169941+Valdorz@users.noreply.github.com>
2021-01-03 21:07:57 +0100

    Added support for the spanish translation
    Firstly, I didn't add any entries at the "emails", neither at "gitlab" or "website" tags, basically because I do not have.
    Anyway, I hope it gives this project a little bit of expansion.

---
+++ [players]/cvf_player/translations/es.lua
@@ -0,0 +1,18 @@
+{
+    "language": "es",
+    "translators": [
+        {
+            "name": "Valdorz",
+            "emails": ["None"],
+            "github": "https://github.com/Valdorz",
+            "gitlab": "None",
+            "website": "None"
+        }
+    ],
+    "translations": {
+        "connect_title": "Cargando cuenta...",
+        "connect_description": "Tu cuenta se está cargando...",
+        "default_job_not_found": "No hemos podido encontrar un trabajo estándar, por favor, contacta con el debido personal del servidor",
+        "player_created": "^2[^7%s^2] ^7La cuenta para ^2\"^7%s^2\" ^7ha sido creada^2!^7"
+    }
+}
